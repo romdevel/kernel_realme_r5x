b8775e676205b24a8ccc6cdec73c33ae9c1fceab
mishrabiswajit660 <mishrabiswajit660@gmail.com>
2023-07-04 09:33:03 +0000

    build.sh: Automated the script to upload the zip to my telegram group

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#
+
 # Compile script for ThunderBolt kernel
 # Copyright (C) 2022-2023 Amritorock.
 
+# Accepting the bot token
+read -p "Enter the bot token: " BOT_TOKEN
+echo -e "\nBot Token have been set successfully !!\n"
+
+# Initializing variables
 SECONDS=0 # builtin bash timer
 ZIPNAME="ThunderBolt-r5x-$(date '+%Y%m%d-%H%M').zip"
 TC_DIR="$HOME/tc/xRageTC-clang"
@@ -45,25 +50,30 @@ make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as 
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
-if [ -d "$AK3_DIR" ]; then
-cp -r $AK3_DIR AnyKernel3
-elif ! git clone -q https://github.com/Amritorock/AnyKernel3; then
-echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
-exit 1
-fi
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-cp out/arch/arm64/boot/dtbo.img AnyKernel3
-rm -f *zip
-cd AnyKernel3
-git checkout master &> /dev/null
-zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
-cd ..
-rm -rf AnyKernel3
-rm -rf out/arch/arm64/boot
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-echo "Zip: $ZIPNAME"
-curl --upload-file $ZIPNAME https://temp.sh/$ZIPNAME; echo
+  if [ -d "$AK3_DIR" ]; then
+    cp -r $AK3_DIR AnyKernel3
+  elif ! git clone -q https://github.com/Amritorock/AnyKernel3; then
+    echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+    exit 1
+  fi
+  cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+  cp out/arch/arm64/boot/dtbo.img AnyKernel3
+  rm -f *zip
+  cd AnyKernel3
+  git checkout master &> /dev/null
+  zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+  cd ..
+  rm -rf AnyKernel3
+  rm -rf out/arch/arm64/boot
+  echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+  echo "Zip: $ZIPNAME"
+
+  # Upload the ZIP file
+  echo -e "\nUploading the ZIP file...\n"
+  curl -F chat_id="-1001635282284" -F document=@"$ZIPNAME" "https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
+  echo -e "\nDone !!\n"
+  
 else
-echo -e "\nCompilation failed!"
-exit 1
+  echo -e "\nCompilation failed!"
+  exit 1
 fi
